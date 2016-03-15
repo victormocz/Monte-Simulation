4cd9bfdd037781e8781eb91542d4647cc10e467a
Chunzhang Mo <victormocz@gmail.com>
2016-03-14 21:55:03 -0500

    add Union class

---
+++ Union.java
@@ -0,0 +1,36 @@
+package Monte;
+
+public class Union {
+	private int[] data;
+	private int[] size;
+	
+	public Union(int N){
+		data = new int[N];
+		size = new int[N];
+		for(int i=0;i<data.length;i++){
+			data[i] = i;
+			size[i] = 1;
+		}
+	}
+	
+	public boolean connected(int node1,int node2){
+		return root(node1)==root(node2);
+	} 
+	public int root(int num){
+		while(num!=data[num]){
+			num = data[num];
+		}
+		return num;
+	}
+	public void union(int node1,int node2){
+		int root1 = root(node1);
+		int root2 = root(node2);
+		if(size[root1]>=size[root2]){
+			data[root2] = data[root1];
+			size[root1]+=size[root2];
+		}else{
+			data[root1] = data[root2];
+			size[root2]+=size[root1];
+		}
+	}
+}
